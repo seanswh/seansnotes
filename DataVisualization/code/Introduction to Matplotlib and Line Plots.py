4c0dcf25326f99c6302745ee96ae0b17072c95c4
sean <sean.sgao@gmail.com>
2019-12-21 18:59:04 +0800

    Track 2 files into repository.
    - modified DataVisualization/code/Introduction to Matplotlib and Line Plots.py
    - modified DataVisualization/code/.vscode/launch.json
    
    Auto commit by GitBook Editor

--- DataVisualization/code/Introduction to Matplotlib and Line Plots.py
+++ DataVisualization/code/Introduction to Matplotlib and Line Plots.py
@@ -3,4 +3,8 @@ import pandas as pd # primary data structure library
 import sys
 
 #Let's download and import our primary Canadian Immigration dataset using pandas read_excel() method. Normally, before we can do that, we would need to download a module which pandas requires to read in excel files. This module is xlrd.
- 
+df_can = pd.read_excel('https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/DV0101EN/labs/Data_Files/Canada.xlsx',
+                       sheet_name='Canada by Citizenship',
+                       skiprows=range(20),
+                       skipfooter=2)
+print ('Data read into a pandas dataframe!')
