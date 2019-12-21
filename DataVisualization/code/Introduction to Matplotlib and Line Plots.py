7903db0aabf8dec1f0a7a90c5ab778caf64e2547
sean <sean.sgao@gmail.com>
2019-12-21 17:19:38 +0800

    Track 3 files into repository.
    - untracked DataVisualization/code/Introduction to Matplotlib and Line Plots.py
    - untracked DataVisualization/code/.vscode/launch.json
    - untracked DataVisualization/code/.vscode/settings.json
    
    Auto commit by GitBook Editor

---
+++ DataVisualization/code/Introduction to Matplotlib and Line Plots.py
@@ -0,0 +1,7 @@
+import numpy as np  # useful for many scientific computing in Python
+import pandas as pd # primary data structure library
+import sys
+
+print(sys.path)
+print(np.__version__)
+print(pd.__version__)
