b0becfb8caeffd29359a484a6151ab57ff86db90
sean <sean.sgao@gmail.com>
2019-12-21 19:03:29 +0800

    Track 1 files into repository.
    - modified DataVisualization/code/Introduction to Matplotlib and Line Plots.py
    
    Auto commit by GitBook Editor

--- DataVisualization/code/Introduction to Matplotlib and Line Plots.py
+++ DataVisualization/code/Introduction to Matplotlib and Line Plots.py
@@ -3,13 +3,13 @@ import pandas as pd # primary data structure library
 import sys
 
 #Let's download and import our primary Canadian Immigration dataset using pandas read_excel() method. Normally, before we can do that, we would need to download a module which pandas requires to read in excel files. This module is xlrd.
-df_can = pd.read_excel('https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/DV0101EN/labs/Data_Files/Canada.xlsx',
+df_can = pd.read_excel('Canada.xlsx',
                        sheet_name='Canada by Citizenship',
                        skiprows=range(20),
                        skipfooter=2)
 print ('Data read into a pandas dataframe!')
 #Let's view the top 5 rows of the dataset using the head() function.
-df_can.head()
+print(df_can.head())
 # tip: You can specify the number of rows you'd like to see as follows: df_can.head(10) 
 #We can also veiw the bottom 5 rows of the dataset using the tail() function.
-df_can.tail()
+print(df_can.tail())
