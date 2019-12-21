ee197c548e89e686e68dac9566b7d3529906acea
sean <sean.sgao@gmail.com>
2019-12-21 17:21:58 +0800

    Track 1 files into repository.
    - modified DataVisualization/code/Introduction to Matplotlib and Line Plots.py
    
    Auto commit by GitBook Editor

--- DataVisualization/code/Introduction to Matplotlib and Line Plots.py
+++ DataVisualization/code/Introduction to Matplotlib and Line Plots.py
@@ -2,6 +2,5 @@ import numpy as np  # useful for many scientific computing in Python
 import pandas as pd # primary data structure library
 import sys
 
-print(sys.path)
-print(np.__version__)
-print(pd.__version__)
+#Let's download and import our primary Canadian Immigration dataset using pandas read_excel() method. Normally, before we can do that, we would need to download a module which pandas requires to read in excel files. This module is xlrd.
+ 
