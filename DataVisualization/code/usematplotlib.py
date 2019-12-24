740988c7ccff136af7a546e5dd8955a005805b20
sean <sean.sgao@gmail.com>
2019-12-24 15:32:03 +0800

    Track 1 files into repository.
    - modified DataVisualization/code/usematplotlib.py
    
    Auto commit by GitBook Editor

--- DataVisualization/code/usematplotlib.py
+++ DataVisualization/code/usematplotlib.py
@@ -12,17 +12,36 @@ df_can = pd.read_excel('Canada.xlsx',
                        skipfooter=2)
 print ('Data read into a pandas dataframe!')
 # useful for plotting later on
-years = list(map(str, range(1980, 2014)))
 df_can.drop(['AREA','REG','DEV','Type','Coverage'], axis=1, inplace=True)
 
 #Let's rename the columns so that they make sense. We can use rename() method by passing in a dictionary of old and new names as follows:
 df_can.rename(columns={'OdName':'Country', 'AreaName':'Continent', 'RegName':'Region'}, inplace=True)
 #print(df_can.describe())
 
-#Before we proceed, notice that the defaul index of the dataset is a numeric range from 0 to 194. This makes it very difficult to do a query by a specific country. For example to search for data on Japan, we need to know the corressponding index value.
-#This can be fixed very easily by setting the 'Country' column as the index using `set_index()` method.
-#df_can.set_index('Country', inplace=True)
+df_can.set_index('Country', inplace=True)
 #print(df_can.head(3))
+#print(df_can.loc['Japan',list(range(1980,1985))])
+
+#Column names that are integers (such as the years) might introduce some confusion. For example, when we are referencing the year 2013, one might confuse that when the 2013th positional index.
+#To avoid this ambuigity, let's convert the column names into strings: '1980' to '2013'.
+df_can.columns = list(map(str, df_can.columns))
+
 #First, we will extract the data series for Haiti.
-#haiti = df_can.loc['Haiti'] # passing in years 1980 - 2013 to exclude the 'total' column
-#print(haiti.head())
+#years = list(map(str, range(1980, 2014)))
+#haiti = df_can.loc['Haiti',years] # passing in years 1980 - 2013 to exclude the 'total' column
+#print(haiti)
+
+#Also, let's label the x and y axis using plt.title(), plt.ylabel(), and plt.xlabel() as follows:
+#haiti.index = haiti.index.map(int) # let's change the index values of Haiti to type integer for plotting
+#haiti.plot(kind='line')
+#plt.title('Immigration from Haiti')
+#plt.ylabel('Number of Immigrants')
+#plt.xlabel('Years')
+# annotate the 2010 Earthquake. 
+# syntax: plt.text(x, y, label)
+#plt.text(2000, 6000, '2010 Earthquake') # see note below
+#plt.show() 
+
+#get China and India Immigration
+df_ci = df_can.loc[['China','India'],years]
+print(df_ci.head())
