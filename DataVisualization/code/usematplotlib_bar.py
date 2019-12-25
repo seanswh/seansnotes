af8e9369d4d41f318cee8422b658c4f9c1659427
sean <sean.sgao@gmail.com>
2019-12-25 10:28:20 +0800

    Track 1 files into repository.
    - modified DataVisualization/code/usematplotlib_bar.py
    
    Auto commit by GitBook Editor

--- DataVisualization/code/usematplotlib_bar.py
+++ DataVisualization/code/usematplotlib_bar.py
@@ -14,13 +14,14 @@ df_can = pd.read_excel('Canada.xlsx',
 print ('Data read into a pandas dataframe!')
 df_can.columns = list(map(str, df_can.columns))
 years = list(map(str, range(1980, 2014)))
+#Let's rename the columns so that they make sense. We can use rename() method by passing in a dictionary of old and new names as follows:
+df_can.rename(columns={'OdName':'Country', 'AreaName':'Continent', 'RegName':'Region'}, inplace=True)
 df_can.set_index('Country', inplace=True)
 df_iceland = df_can.loc['iceland',years]
 # step 2: plot data
 df_iceland.plot(kind='bar', figsize=(10, 6))
-
 plt.xlabel('Year') # add to x-label to the plot
 plt.ylabel('Number of immigrants') # add y-label to the plot
 plt.title('Icelandic immigrants to Canada from 1980 to 2013') # add title to the plot
-
 plt.show()
+print("finished")
