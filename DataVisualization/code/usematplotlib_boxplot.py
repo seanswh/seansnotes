ea7e3404516fff225de74650bcfe61956e689be0
sean <sean.sgao@gmail.com>
2019-12-26 15:19:26 +0800

    Track 1 files into repository.
    - modified DataVisualization/code/usematplotlib_boxplot.py
    
    Auto commit by GitBook Editor

--- DataVisualization/code/usematplotlib_boxplot.py
+++ DataVisualization/code/usematplotlib_boxplot.py
@@ -5,4 +5,4 @@
     #Second quartile (Median): Middle number of the (sorted) dataset.
     #Third quartile: Middle number between median and maximum.
     #Maximum: Highest number in the dataset.
-To make a box plot, we can use kind=box in plot method invoked on a pandas series or dataframe.
+#To make a box plot, we can use kind=box in plot method invoked on a pandas series or dataframe.
