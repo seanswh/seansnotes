d66b8fc08e79e763b7756bd34db7067fcc60e987
sean <sean.sgao@gmail.com>
2019-12-27 10:26:10 +0800

    Track 1 files into repository.
    - modified DataVisualization/code/usematplotlib_waffle.py
    
    Auto commit by GitBook Editor

--- DataVisualization/code/usematplotlib_waffle.py
+++ DataVisualization/code/usematplotlib_waffle.py
@@ -10,6 +10,7 @@ df_can = pd.read_excel('Canada.xlsx',
                        skiprows=range(20),
                        skipfooter=2)
 print ('Data read into a pandas dataframe!')
+
 df_can.drop(['AREA', 'REG', 'DEV', 'Type', 'Coverage'], axis=1, inplace=True)
 # let's rename the columns so that they make sense
 df_can.rename (columns = {'OdName':'Country', 'AreaName':'Continent','RegName':'Region'}, inplace = True)
@@ -25,3 +26,34 @@ df_can['Total'] =  df_can.sum (axis = 1)
 
 # years that we will be using in this lesson - useful for plotting later on
 years = list(map(str, range(1980, 2014)))
+#Waffle Charts 
+    #A waffle chart is an interesting visualization that is normally created to display progress toward goals. It is commonly an effective option when you are trying to add interesting visualization features to a visual that consists mainly of cells, such as an Excel dashboard.
+# let's create a new dataframe for these three countries 
+df_dsn = df_can.loc[['Denmark', 'Norway', 'Sweden'], :]
+print(df_dsn)
+#Unfortunately, unlike R, waffle charts are not built into any of the Python visualization libraries. Therefore, we will learn how to create them from scratch.
+# compute the proportion of each category with respect to the total
+
+#Step 1. The first step into creating a waffle chart is determing the proportion of each category with respect to the total.
+total_values = sum(df_dsn['Total'])
+category_proportions = [(float(value) / total_values) for value in df_dsn['Total']]
+print(category_proportions)
+# print out proportions
+for i, proportion in enumerate(category_proportions):
+    print (df_dsn.index.values[i] + ': ' + str(proportion))
+
+#Step 2. The second step is defining the overall size of the waffle chart.
+width = 40 # width of chart
+height = 10 # height of chart
+total_num_tiles = width * height # total number of tiles
+
+print ('Total number of tiles is ', total_num_tiles)
+#Step 3. The third step is using the proportion of each category to determe it respective number of tiles
+
+# compute the number of tiles for each catagory
+tiles_per_category = [round(proportion * total_num_tiles) for proportion in category_proportions]
+​
+# print out number of tiles per category
+for i, tiles in enumerate(tiles_per_category):
+    print (df_dsn.index.values[i] + ': ' + str(tiles))
+#Step 4. The fourth step is creating a matrix that resembles the waffle chart and populating it.
