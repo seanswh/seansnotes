fe636bb19a2bc6b50f44aab10e519e87635ffd95
sean <sean.sgao@gmail.com>
2019-12-27 09:10:05 +0800

    Track 5 files into repository.
    - modified DataVisualization/code/usematplotlib_waffle.py
    - untracked DataVisualization/code/notebooks/DV0101EN-1-1-1-Introduction-to-Matplotlib-and-Line-Plots-py-v2.0.ipynb
    - untracked DataVisualization/code/notebooks/DV0101EN-2-2-1-Area-Plots-Histograms-and-Bar-Charts-py-v2.0.ipynb
    - untracked DataVisualization/code/notebooks/DV0101EN-2-3-1-Pie-Charts-Box-Plots-Scatter-Plots-and-Bubble-Plots-py-v2.0.ipynb
    - untracked DataVisualization/code/notebooks/DV0101EN-3-4-1-Waffle-Charts-Word-Clouds-and-Regression-Plots-py-v2.0.ipynb
    
    Auto commit by GitBook Editor

--- DataVisualization/code/usematplotlib_waffle.py
+++ DataVisualization/code/usematplotlib_waffle.py
@@ -1,6 +1,9 @@
 import numpy as np  # useful for many scientific computing in Python
 import pandas as pd # primary data structure library
 from PIL import Image # converting images into arrays
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches # needed for waffle Charts
 
 df_can = pd.read_excel('Canada.xlsx',
                        sheet_name='Canada by Citizenship',
@@ -8,3 +11,17 @@ df_can = pd.read_excel('Canada.xlsx',
                        skipfooter=2)
 print ('Data read into a pandas dataframe!')
 df_can.drop(['AREA', 'REG', 'DEV', 'Type', 'Coverage'], axis=1, inplace=True)
+# let's rename the columns so that they make sense
+df_can.rename (columns = {'OdName':'Country', 'AreaName':'Continent','RegName':'Region'}, inplace = True)
+
+# for sake of consistency, let's also make all column labels of type string
+df_can.columns = list(map(str, df_can.columns))
+
+# set the country name as index - useful for quickly looking up countries using .loc method
+df_can.set_index('Country', inplace = True)
+
+# add total column
+df_can['Total'] =  df_can.sum (axis = 1)
+
+# years that we will be using in this lesson - useful for plotting later on
+years = list(map(str, range(1980, 2014)))
