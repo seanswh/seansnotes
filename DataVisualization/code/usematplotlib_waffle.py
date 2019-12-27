a993042b69d7604979e932904d93ab959f53853a
sean <sean.sgao@gmail.com>
2019-12-27 16:34:35 +0800

    Track 3 files into repository.
    - modified DataVisualization/code/usematplotlib_waffle.py
    - untracked DataVisualization/code/usematplotlib_wordclouds.py
    - untracked DataVisualization/code/alice_novel.txt
    
    Auto commit by GitBook Editor

--- DataVisualization/code/usematplotlib_waffle.py
+++ DataVisualization/code/usematplotlib_waffle.py
@@ -52,8 +52,69 @@ print ('Total number of tiles is ', total_num_tiles)
 
 # compute the number of tiles for each catagory
 tiles_per_category = [round(proportion * total_num_tiles) for proportion in category_proportions]
-​
+
 # print out number of tiles per category
 for i, tiles in enumerate(tiles_per_category):
     print (df_dsn.index.values[i] + ': ' + str(tiles))
 #Step 4. The fourth step is creating a matrix that resembles the waffle chart and populating it.
+# initialize the waffle chart as an empty matrix
+waffle_chart = np.zeros((height, width))
+
+# define indices to loop through waffle chart
+category_index = 0
+tile_index = 0
+
+# populate the waffle chart
+for col in range(width):
+    for row in range(height):
+        tile_index += 1
+
+        # if the number of tiles populated for the current category is equal to its corresponding allocated tiles...
+        if tile_index > sum(tiles_per_category[0:category_index]):
+            # ...proceed to the next category
+            category_index += 1       
+            
+        # set the class value to an integer, which increases with class
+        waffle_chart[row, col] = category_index
+        
+print(waffle_chart)
+#Step 5. Map the waffle chart matrix into a visual.
+# use matshow to display the waffle chart
+# instantiate a new figure object
+
+# use matshow to display the waffle chart
+colormap = plt.cm.coolwarm
+plt.matshow(waffle_chart, cmap=colormap)
+plt.colorbar()
+
+# get the axis
+ax = plt.gca()
+
+# set minor ticks
+ax.set_xticks(np.arange(-.5, (width), 1), minor=True)
+ax.set_yticks(np.arange(-.5, (height), 1), minor=True)
+    
+# add gridlines based on minor ticks
+ax.grid(which='minor', color='w', linestyle='-', linewidth=2)
+
+plt.xticks([])
+plt.yticks([])
+
+# compute cumulative sum of individual categories to match color schemes between chart and legend
+values_cumsum = np.cumsum(df_dsn['Total'])
+total_values = values_cumsum[len(values_cumsum) - 1]
+
+# create legend
+legend_handles = []
+for i, category in enumerate(df_dsn.index.values):
+    label_str = category + ' (' + str(df_dsn['Total'][i]) + ')'
+    color_val = colormap(float(values_cumsum[i])/total_values)
+    legend_handles.append(mpatches.Patch(color=color_val, label=label_str))
+
+# add legend to chart
+plt.legend(handles=legend_handles,
+           loc='lower center', 
+           ncol=len(df_dsn.index.values),
+           bbox_to_anchor=(0., -0.2, 0.95, .1)
+          )
+plt.show()
---
+++ DataVisualization/code/usematplotlib_wordclouds.py
@@ -0,0 +1,3 @@
+#Word Clouds
+    #Word clouds (also known as text clouds or tag clouds) work in a simple way: the more a specific word appears in a source of textual data (such as a speech, blog post, or database), the bigger and bolder it appears in the word cloud.
+    #Luckily, a Python package already exists in Python for generating word clouds. The package, called word_cloud was developed by Andreas Mueller. You can learn more about the package by following this link.
